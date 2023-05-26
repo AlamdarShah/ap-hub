1028443911017fe8851a4b79f21a138750a853ed
AlamdarShah <alamdarshah702@gmail.com>
2023-05-26 23:04:12 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Mainsection from './components/Mainsection';
+import Navbar  from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Mainsection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mainsection.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Mainsection = () => {
+    return (
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='first-div col-lg-6 col-sm-12' >
+                      <button>submit</button>
+                    </div>
+                    <div className='second-div col-lg-6 col-sm-12'><img src="main" alt="main img" /></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Mainsection;
