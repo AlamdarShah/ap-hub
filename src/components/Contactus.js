d8e2230851451691f8feb2edcf715a7276460ab0
IshaNoor91 <ishanoor9192@gmail.com>
2023-05-28 00:24:40 +0500

    create Contactus page

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
+
 export const Contactus = () => {
   return (
-    <div><h1>this is contact us page</h1></div>
+    <div className="cont" >
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img  src="https://is-enterprises.com/wp-content/uploads/2018/05/contact-1.jpg" className="d-block w-100 crsl-img" alt="..." />
+      
+    </div>
+    </div>
+    <div class="card">
+    <h2>Contact Information</h2>
+    <div class="contact-info">
+      <i class="fas fa-envelope"></i>
+      <span>example@example.com</span>
+    </div>
+    <div class="contact-info">
+      <i class="fas fa-phone"></i>
+      <span>+1234567890</span>
+    </div>
+    <div class="contact-info">
+      <i class="fas fa-map-marker-alt"></i>
+      <span>123 Street, City, Country</span>
+    </div>
+  </div>
+ 
+  <script src="https://kit.fontawesome.com/your-font-awesome-kit.js" crossorigin="anonymous"></script>
+    </div>
   )
 }
