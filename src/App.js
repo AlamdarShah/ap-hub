2e06e81115bb9e86e2150e877f9e76019234e453
Raheel309 <raheel309.baig@gmail.com>
2023-05-28 01:54:06 +0500

    product page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Mainsection from './components/Mainsection';
 import Navbar  from './components/Navbar';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Product/>
      <Mainsection />
     </div>
   );
